e7c31aef5dcced25eb64718cbc429e75ef7cf1b6
MrSamy91 <samy.ajouid@gmail.com>
2025-02-14 15:20:00 +0100

    pushing exo 2

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 """Module for filtering sensitive information from log messages."""
 
+import logging
 import re
+from typing import List
 
 
 def filter_datum(fields: list, redaction: str, message: str, separator: str) -> str:
@@ -21,3 +23,33 @@ def filter_datum(fields: list, redaction: str, message: str, separator: str) -> 
         pattern = f"(?<={field}=)[^{separator}]*"
         message = re.sub(pattern, redaction, message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """Initialize the RedactingFormatter with fields to redact.
+        
+        Args:
+            fields: List of strings representing fields to redact
+        """
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Format the log record while redacting specified fields.
+        
+        Args:
+            record: LogRecord instance containing the log message
+            
+        Returns:
+            Formatted log message with sensitive information redacted
+        """
+        record.msg = filter_datum(self.fields, self.REDACTION, record.msg, self.SEPARATOR)
+        return super().format(record)
